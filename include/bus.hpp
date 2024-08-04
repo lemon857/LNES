5d676753bcda1ae59be9cbf57b127c94046e9820
lemon857 <lemon857.an@gmail.com>
2024-08-04 13:30:53 +0500

    add: memory and operations on it in bus

--- include/bus.hpp
+++ include/bus.hpp
@@ -1,22 +1,23 @@
 #ifndef _BUS_HPP_
 #define _BUS_HPP_
 
-#include "cpu.hpp"
 #include "mem.hpp"
 
 class bus
 {
 public:
-    bus();
-    ~bus();
+    bus() = delete;
+    bus(bus&&) = delete;
+    bus operator=(bus&&) = delete;
+    ~bus() = delete;
 
-    void write(u16 addr, u8 data);
-    u8 read(u16 addr);
+    static void write(u16 addr, u8 data);
+    static u8 read(u16 addr);
 
-    void reset();
-    void insertCartrige();
+    static void reset();
+    static void insertCartridge();
 private:
-
+    static mem memory;
 };
 
 #endif // _BUS_HPP_
--- include/mem.hpp
+++ include/mem.hpp
@@ -1,8 +1,26 @@
 #ifndef _MEM_HPP_
 #define _MEM_HPP_
 
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned int u32;
+using u8 = unsigned char;
+using u16 = unsigned short;
+using u32 = unsigned int;
+
+struct mem
+{
+    u8 data[0xFFFF];
+    void init()
+    {
+        for (u16 i = 0; i < 0xFFFF; i++) data[i] = 0;
+    }
+    u8 operator[](u16 index) const
+    {
+        return data[index];    
+    }
+    u8& operator[](u16 index)
+    {
+        return data[index];    
+    }
+};
+
 
 #endif // _MEM_HPP_
